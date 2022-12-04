ef4d4ff8c1d821c8a461db6af5e33b56a789aa74
KimaniiiG <113220605+KimaniiiG@users.noreply.github.com>
2022-12-03 23:45:01 -0500

    Add files via upload

---
+++ MotionDetection_Kimani.py
@@ -0,0 +1,100 @@
+import threading
+import winsound
+from email.message import EmailMessage
+import smtplib
+import cv2
+import imutils
+
+def retrieve_input():
+    global EmailAddress
+    EmailAddress = EmailAddressBox.get()
+
+
+print("Alarm System Started")
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4000)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 4000)
+
+_, start_frame = cap.read()
+start_frame = imutils.resize(start_frame, width=1200)
+start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
+start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
+
+alarm = False
+alarm_mode = False
+alarm_counter = 0
+
+
+def email_alert(Subject, body, to):
+		msg = EmailMessage()
+		msg.set_content(body)
+		msg['subject'] = Subject
+		msg['to'] = to
+
+		user = "DroneSurvilenceProject@gmail.com"
+		msg['from'] = user
+		password = "jduhshcgtkyvwplb"
+
+		server = smtplib.SMTP("smtp.gmail.com", 587)
+		server.starttls()
+		server.login(user, password)
+		server.send_message(msg)
+
+		server.quit()
+
+
+def beep_alarm():
+
+	global alarm
+	for _ in range(5):
+		if not alarm_mode:
+			break
+		print("ALARM SET OFF")
+		email_alert("Motion Detection Warning", "An Alarm Has Been Triggered By Your Survalince System", "Kimguchu30@gmail.com")
+		winsound.Beep(2500,1000)
+	alarm = False
+
+
+while True:
+
+	_, frame = cap.read()
+	frame = imutils.resize(frame, width=1200)
+
+	if alarm_mode:
+		frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
+
+		difference = cv2.absdiff(frame_bw, start_frame)
+		threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
+		start_frame = frame_bw
+
+		if threshold.sum() > 1000:
+			alarm_counter += 1
+
+		else:
+			if alarm_counter > 0:
+				alarm_counter -= 1
+
+		cv2.imshow("Cam", threshold)
+	else:
+		cv2.imshow("Cam", frame)
+
+	if alarm_counter > 20:
+		if not alarm:
+			alarm = True
+			threading.Thread(target=beep_alarm).start()
+
+	key_pressed = cv2.waitKey(30)
+
+	if key_pressed == ord("a"):
+		alarm_mode = not alarm_mode
+		alarm_counter = 0
+
+	if key_pressed == ord("s"):
+		alarm_mode = False
+		break
+
+cap.release()
+cv2.destroyAllWindows()
